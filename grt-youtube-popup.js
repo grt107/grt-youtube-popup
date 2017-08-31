1d8b6370229d618863e572909de362e70df9a1e2
grt107 <31504327+grt107@users.noreply.github.com>
2017-08-31 15:48:56 +0200

    main plugin file

---
+++ grt-youtube-popup.js
@@ -0,0 +1,56 @@
+/*!
+ * GRT Youtube Popup - jQuery Plugin
+ * Version: 1.0
+ * Author: GRT107
+ *
+ * Copyright (c) 2017 GRT107
+ * Released under the MIT license
+*/
+
+(function ( $ ) {
+
+	$.fn.grtyoutube = function( options ) {
+
+		return this.each(function() {
+
+			// Get video ID
+			var getvideoid = $(this).attr("youtubeid");
+
+			// Default options
+			var settings = $.extend({
+				videoID: getvideoid,
+				autoPlay: true
+			}, options );
+
+				// Convert some values
+				if(settings.autoPlay === true) { settings.autoPlay = 1 } else { settings.autoPlay = 0 }
+
+				// Initialize on click
+				if(getvideoid) {
+					$(this).on( "click", function() {
+						 $("body").append('<div class="grtvideo-popup">'+
+											'<div class="grtvideo-popup-content">'+
+												'<span class="grtvideo-popup-close">X</span>'+
+												'<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/'+settings.videoID+'?rel=0&wmode=transparent&autoplay='+settings.autoPlay+'&iv_load_policy=3" allowfullscreen frameborder="0"></iframe>'+
+											'</div>'+
+										'</div>');
+					});
+				}
+
+				// Close the box on click or escape
+				$(this).on('click', function (event) {
+		            event.preventDefault();
+					$(".grtvideo-popup-close, .grtvideo-popup").click(function(){
+						$(".grtvideo-popup").remove();
+					});
+				});
+
+				$(document).keyup(function(event) {
+					if (event.keyCode == 27){
+						$(".grtvideo-popup").remove();
+					}
+				});
+			});
+	};
+
+}( jQuery ));
